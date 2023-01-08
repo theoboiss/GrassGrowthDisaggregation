078205737f60406bee7be514f95c9dff671af473
Théo <48034060+theo-boi@users.noreply.github.com>
2023-01-08 23:35:38 +0100

    Fix: Data preprocessing + Tuners

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -24,64 +24,69 @@ def splitDatasetGeo(dataset):
     return X_train, Y_train, X_test, Y_test
 
 
+
 # Adapt for algorithms
 
-def convertDatasetSVR(X, Y, nb_annees = 5, window_size = 4, nb_decades = 37, progression_step = 20):
+def convertDatasetALGO(convertSample, editYALGO, X, Y, nb_annees = 5, window_size = 4, nb_decades = 37, progression_step = 20):
     new_X, new_Y = list(), list()
     last_progression = 0
-    data_id_size = nb_decades*nb_annees # number of decades per year # PAS BATCH
+    
+    # Initialise the daily growths variables only
+    Y_feats = editYALGO(Y.copy(), nb_decades, window_size)
 
     print(0, '%')
-    for data_id_index in range(0, len(X) - data_id_size, data_id_size): # for each batch
-        batch_end_index = data_id_index + data_id_size
-        # build samples by concatenating all entries that fit in the window
-        for index in range(data_id_index, batch_end_index - window_size): # for each window in the batch
-            x_c = X.iloc[index:(index + window_size), :] # concatenate climate data of entries that fit in the window
-            x_g = Y.iloc[index:(index + window_size - 1), 0] # concatenate daily growth of the beginning of the window
-            x_c = x_c.values.reshape(-1)
-            x_g = x_g.values.reshape(-1)
-            x = list(x_c) + list(x_g)
-
-            y = Y.iloc[index + window_size, 0] # daily growth of the end of the window
+    for years_index in range(0, len(X) - nb_decades - window_size, nb_decades): # for each year
+        # build samples by concatenating all the data in the window
+        for window_index in range(years_index, years_index + nb_decades): # for each window
+            
+            x = convertSample(X, Y_feats, window_index, window_size)
+            y = Y.iloc[window_index + window_size] # daily growth of the end of the window
+    
             new_X.append(x)
             new_Y.append(y)
             
-        progression = data_id_index / (len(X) - data_id_size) * 100
+        progression = years_index / (len(X) - nb_decades) * 100
         if progression - last_progression > 1 and not int(progression) % progression_step:
             print(int(progression), '%')
             last_progression = progression
     print(100, '%')
     return new_X, new_Y
 
+
+def editYSVR(Y, nb_decades, window_size, last_daily_growth= 7.17, first_daily_growths= 9.57):
+    Y_feats = Y
+    Y_feats.loc[Y_feats.index % nb_decades == (nb_decades-1)] = last_daily_growth # init of the last daily growth of each year
+    Y_feats.loc[Y_feats.index % nb_decades < (window_size-1)] = first_daily_growths # init of the first daily growths of each year
+    return Y_feats
+
+def convertSampleSVR(X, Y, window_index, window_size):
+    x_c = X.iloc[window_index:(window_index + window_size)] # concatenate climate data of entries that fit in the window
+    x_g = Y.iloc[window_index:(window_index + window_size - 1)] # concatenate daily growth of the beginning of the window
+    x_c = x_c.values.reshape(-1)
+    x_g = x_g.values.reshape(-1)
+
+    x = np.concatenate((x_c, x_g), axis = 0)
+    return x
+
+def convertDatasetSVR(X, Y, nb_annees = 5, window_size = 4, nb_decades = 37, progression_step = 20):
+    return convertDatasetALGO(convertSampleSVR, editYSVR, X, Y, nb_annees, window_size, nb_decades, progression_step)
+
+
+def editYLSTM(Y, nb_decades, window_size, last_daily_growth= 7.17, first_daily_growths= 9.57):
+    Y_feats = Y
+    
+    Y_feats.loc[Y_feats.index % nb_decades == (nb_decades-1)] = last_daily_growth # init of the last daily growth of each year
+    Y_feats.loc[Y_feats.index % nb_decades < (window_size-1)] = first_daily_growths # init of the first daily growths of each year
+    Y_feats = pd.concat([pd.DataFrame([[last_daily_growth]], columns= Y_feats.columns), Y_feats])
+    return Y_feats
+
+def convertSampleLSTM(X, Y, window_index, window_size):
+    x_c = X.iloc[window_index+1:(window_index + window_size)+1] # concatenate climate data of entries that fit in the window
+    x_g = Y.iloc[window_index  :(window_index + window_size)] # concatenate daily growth of the beginning of the window
+    x_g = x_g.values.reshape(window_size, 1)
+    
+    x = np.concatenate((x_c, x_g), axis = 1)
+    return x
 
 def convertDatasetLSTM(X, Y, nb_annees = 5, window_size = 4, nb_decades = 37, progression_step = 20):
-    new_X, new_Y = list(), list()
-    last_progression = 0
-    data_id_size = nb_decades*nb_annees # number of decades per year # PAS BATCH
-    
-    print(0, '%')
-    for data_id_index in range(0, len(X) - data_id_size, data_id_size): # for each batch
-        batch_end_index = data_id_index + data_id_size
-        # build samples by concatenating all entries that fit in the window
-        for index in range(data_id_index, batch_end_index - window_size): # for each window in the batch # for each window in the batch
-            #x_c = X.iloc[index+1] # concatenate climate data of entries that fit in the window
-            #x_g = Y.iloc[index] # concatenate daily growth of the beginning of the window
-            #x_c = x_c.values.reshape(-1)
-            #x_g = x_g.values.reshape(-1)
-            #x = list(x_c) + list(x_g)
-            x = list()
-            for i in range(window_size):
-                x_i = np.concatenate((X.iloc[index+1+i].values.reshape(-1), \
-                                        Y.iloc[index+i].values.reshape(-1)),axis = 0)
-                x.append(x_i)
- 
-            y = Y.iloc[index+window_size] # daily growth of the end of the window
-            new_X.append(x)
-            new_Y.append(y)
-            
-        progression = data_id_index / (len(X) - data_id_size) * 100
-        if progression - last_progression > 1 and not int(progression) % progression_step:
-            print(int(progression), '%')
-            last_progression = progression
-    print(100, '%')
-    return new_X, new_Y
+    return convertDatasetALGO(convertSampleLSTM, editYLSTM, X, Y, nb_annees, window_size, nb_decades, progression_step)
--- data/window.py
+++ data/window.py
@@ -1,28 +1,34 @@
 import numpy as np
 
 class SlidingWindow:
-    def __init__(self, X: np.ndarray):
+    def __init__(self, X: np.ndarray, reset_cycle= 37):
         self.X = X
-        self.it = 0
+        self.it = 1
         self.init_seq_size = X.shape[1]
         self.values = X[0]
+        self.reset_cycle = reset_cycle
         
     def next(self, pred):
-        if self.it + 1 == len(self.X):
+        if self.it == len(self.X):
             return 0
+        new_values = self.X[self.it, :].copy()
+        if self.it % self.reset_cycle: # Not the start of the cycle
+            if self.init_seq_size > 0:
+                self.init_seq_size -= 1
+                new_values[self.init_seq_size:-1, -1] = self.values[self.init_seq_size+1:, -1]
+            else:
+                new_values[:-1, -1] = self.values[1:, -1]
+            new_values[-1, -1] = pred
+            self.values = new_values
+        else: # Start of the cycle
+            # Reinit the values
+            self.init_seq_size = self.X.shape[1]
+            self.values = self.X[self.it]        
         self.it += 1
-        new_values = np.copy(self.X[self.it, :])
-        if self.init_seq_size > 1:
-            self.init_seq_size -= 1
-            new_values[self.init_seq_size-1:-1, -1] = self.values[self.init_seq_size:, -1]
-        else:
-            new_values[:-1, -1] = self.values[1:, -1]
-        new_values[-1, -1] = pred
-        self.values = new_values
         return self.it
     
     def __str__(self):
-        return str(("iterator: " + str(self.it), "init_seq_size: " + str(self.init_seq_size), self.values))
+        return str(("iterator: " + str(self.it-1), "init_seq_size: " + str(self.init_seq_size), self.values))
     
     def __repr__(self):
         return str(self)
